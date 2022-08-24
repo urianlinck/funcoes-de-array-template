e76a06ddbeafda376a17c081be31bf5e54164e58
João Vinicius Alves <jvp.alves@hotmail.com>
2022-08-24 16:00:55 -0300

    Update script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+const pokemons = [
+   { nome: "Bulbasaur", tipo: "grama", vida: 40 },
+   { nome: "Bellsprout", tipo: "grama", vida: 20 },
+   { nome: "Charmander", tipo: "fogo": vida: 35 },
+   { nome: "Vulpix", tipo: "fogo", vida: 25 },
+   { nome: "Squirtle", tipo: "água", vida: 45 },
+   { nome: "Psyduck", tipo: "água", vida: 25 },
+]
