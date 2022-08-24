41a9ec34dcb863eaddfb2d86f54f4df0f25bd7b6
Tayanne <tayh.snovaes@gmail.com>
2022-08-24 16:12:35 -0300

    [fix]

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const pokemons = [
-   { nome: "Bulbasaur", tipo: "grama", vida: 40 },
-   { nome: "Bellsprout", tipo: "grama", vida: 20 },
-   { nome: "Charmander", tipo: "fogo": vida: 35 },
-   { nome: "Vulpix", tipo: "fogo", vida: 25 },
-   { nome: "Squirtle", tipo: "água", vida: 45 },
-   { nome: "Psyduck", tipo: "água", vida: 25 },
+  { nome: 'Bulbasaur', tipo: 'grama', vida: 40 },
+  { nome: 'Bellsprout', tipo: 'grama', vida: 20 },
+  { nome: 'Charmander', tipo: 'fogo', vida: 35 },
+  { nome: 'Vulpix', tipo: 'fogo', vida: 25 },
+  { nome: 'Squirtle', tipo: 'água', vida: 45 },
+  { nome: 'Psyduck', tipo: 'água', vida: 25 },
 ]
